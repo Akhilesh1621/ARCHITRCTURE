175ba71a1e82813348d838a7ee30980319abe7c3
akhilesh <akhileshdevop@gmail.com>
2020-09-28 14:11:35 +0530

    title change

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Archi',
+      title: 'Flutter Architecture',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
